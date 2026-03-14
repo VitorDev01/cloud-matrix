e7124e3a0b112f1f9b862211865793be430d726f
Vitor Oliveira <victorskw89@gmail.com>
2026-03-14 11:24:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,12 @@ alert("ACESSO NEGADO");
 }
 
 }
+
+const audio = new Audio("matrix.mp3");
+
+audio.loop = true;
+audio.volume = 0.2;
+
+document.addEventListener("click", () => {
+audio.play();
+});
