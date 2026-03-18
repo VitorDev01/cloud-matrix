ca8a863ef953271a2546e1cf76c49f9c85c34f51
Vitor Oliveira <victorskw89@gmail.com>
2026-03-18 19:24:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,11 +54,3 @@ alert("ACESSO NEGADO");
 }
 
 }
-/*const audio = new Audio("matrix1.mp3");
-audio.volume = 0.2;
-audio.loop = true;
-
-// Toca quando o usuário clicar em qualquer lugar da tela
-document.addEventListener("click", () => {
-    audio.play();
-}, { once: true });
